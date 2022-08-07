bfe1da6ef4492750466c9312494b98063639eb5a
sunDalik <dali1999@mail.ru>
2022-08-07 14:56:15 +0300

    Update README and refactor getItemOffset

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -19,14 +19,15 @@ class AppComponent implements OnInit {
   }
 
   String formatDate(String milliseconds) {
-    DateTime date =
-        DateTime.fromMillisecondsSinceEpoch(int.parse(milliseconds));
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(int.parse(milliseconds));
     return '${date.hour.toString().padLeft(2, '0')}.${date.minute.toString().padLeft(2, '0')}.${date.second.toString().padLeft(2, '0')}\n${date.day.toString().padLeft(2, '0')}.${date.month.toString().padLeft(2, '0')}.${date.year}';
   }
 
   String getItemOffset(var item, var obj, int width){
-    var offset = (((int.parse(item['timestamp']) - int.parse(obj['items'][0]['timestamp'])) / (int.parse(obj['items'][obj['items'].length - 1]['timestamp']) - int.parse(obj['items'][0]['timestamp']))) * width).toString();
-    print(offset);
+    int itemTimestamp = int.parse(item['timestamp']);
+    int starTimestamp = int.parse(obj['items'][0]['timestamp']);
+    int endTimestamp = int.parse(obj['items'][obj['items'].length - 1]['timestamp']);
+    String offset = (((itemTimestamp - starTimestamp) / (endTimestamp - starTimestamp)) * width).toString();
     return offset;
   }
 }
