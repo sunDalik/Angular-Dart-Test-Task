a230de3dc2fa33b6ef60c07bf94e15df8997a839
sunDalik <dali1999@mail.ru>
2022-08-07 16:43:47 +0300

    Add second object

--- lib/mock_data.dart
+++ lib/mock_data.dart
@@ -22,6 +22,25 @@ var MOCK_DATA = '''{
         },
         {"state": "Выгружено", "timestamp": "1699804988105", "progress": "0%"}
       ]
+    },
+
+    {
+      "obj_name": "Объект 2",
+      "obj_id": "12345",
+      "items": [
+        {
+          "state": "Оформление",
+          "timestamp": "1754004988105",
+          "progress": "100%"
+        },
+        {
+          "state": "Сборка",
+          "timestamp": "1763804988105",
+          "progress": "100%"
+        },
+        {"state": "Доставка", "timestamp": "1770804988105", "progress": "28%"},
+        {"state": "Получено", "timestamp": "1799804988105", "progress": "0%"}
+      ]
     }
   ]
 }''';
